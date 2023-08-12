c1c9448929da5803eedd32d6894fbd504352c856
tathaf16 <142090780+tathaf16@users.noreply.github.com>
2023-08-12 16:10:01 +0530

    Add files via upload

---
+++ Data Science Tools.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notrbook, Data Science Tools and Ecosystem are compiled
+
+# Popular Languages data science uses
+# 1. python
+# 2. R
+# 3. SQL
+# 4. C++
+# 5. Java etc..
+
+# Data Science Libraries
+# 1. NumPy
+# 2. Pandas
+# 3. Matplotlib and Seaborn
+# 4. Scikit-learn
+# 5. TensorFlow and PyTorch
+# 6. StatsModels
+# 7. NLTK and spaCy
+# 8. Beautiful Soup and Scrapy
+
+# |Data Science Tools|
+# |---|
+# |Apache Hadoop|
+# |TensorFlow|
+# |PowerBI|
+# |SQL|
+
+# ### Few examples of arithmetic expressions
+
+# In[4]:
+
+
+#to multiply and then add integers
+(8*7)+9
+
+
+# In[6]:
+
+
+#example of conversion of min into hourse
+420/60
+
+
+# #### Objectives
+# 1. List popular libraries for Data Science
+# 2. List popular languages for Data Science
+# 3. List of well known Data Science Tools
+
+# ## Author Name
+# Tathagat Fulzele
+
+# In[ ]:
+
+
+
+
